1e67a0260256194304d4ce68014ca07fb265024d
Manu Morante <manuel.morante@betvictor.com>
2016-12-21 16:16:40 +0100

    Initial

---
+++ source/javascripts/application.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+var Test = () => {
+  return(<div>Entra</div>);
+};
+
+ReactDOM.render(
+<Test />,
+  document.getElementById('root')
+);
